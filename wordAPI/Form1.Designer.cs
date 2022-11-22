06a9498bd8f3a3ba6bc916515192699d26e6d2dd
Mihai <mihaismoleac2002@gmail.com>
2022-11-22 20:48:25 +0200

    Add project files.

---
+++ wordAPI/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace wordAPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.import = new System.Windows.Forms.Button();
+            this.textToImport = new System.Windows.Forms.RichTextBox();
+            this.textToExport = new System.Windows.Forms.RichTextBox();
+            this.export = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // import
+            // 
+            this.import.Location = new System.Drawing.Point(413, 24);
+            this.import.Name = "import";
+            this.import.Size = new System.Drawing.Size(220, 29);
+            this.import.TabIndex = 1;
+            this.import.Text = "IMPORT TEXT FROM WORD";
+            this.import.UseVisualStyleBackColor = true;
+            this.import.Click += new System.EventHandler(this.import_Click);
+            // 
+            // textToImport
+            // 
+            this.textToImport.Location = new System.Drawing.Point(413, 59);
+            this.textToImport.Name = "textToImport";
+            this.textToImport.Size = new System.Drawing.Size(375, 234);
+            this.textToImport.TabIndex = 2;
+            this.textToImport.Text = "";
+            // 
+            // textToExport
+            // 
+            this.textToExport.Location = new System.Drawing.Point(12, 59);
+            this.textToExport.Name = "textToExport";
+            this.textToExport.Size = new System.Drawing.Size(375, 234);
+            this.textToExport.TabIndex = 4;
+            this.textToExport.Text = "";
+            // 
+            // export
+            // 
+            this.export.Location = new System.Drawing.Point(12, 24);
+            this.export.Name = "export";
+            this.export.Size = new System.Drawing.Size(158, 29);
+            this.export.TabIndex = 3;
+            this.export.Text = "EXPORT TO WORD";
+            this.export.UseVisualStyleBackColor = true;
+            this.export.Click += new System.EventHandler(this.export_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 325);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(102, 113);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(262, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(262, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "File Name";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 299);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(180, 24);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Put image at the end ?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textToExport);
+            this.Controls.Add(this.export);
+            this.Controls.Add(this.textToImport);
+            this.Controls.Add(this.import);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Button import;
+        private RichTextBox textToImport;
+        private RichTextBox textToExport;
+        private Button export;
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+        private Label label1;
+        private CheckBox checkBox1;
+        private FontDialog fontDialog1;
+    }
+}
---
+++ wordAPI/Form1.cs
@@ -0,0 +1,98 @@
+using System.Security.Cryptography;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+using Spire.Doc;
+using Spire.Doc.Documents;
+using Spire.Doc.Fields;
+using Document = Spire.Doc.Document;
+using Paragraph = Spire.Doc.Documents.Paragraph;
+using Range = Microsoft.Office.Interop.Word.Range;
+using Section = Spire.Doc.Section;
+
+namespace wordAPI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void import_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { ValidateNames = true, Multiselect = false, Filter = "Word 97-2003|* .doc|Word Document|*.docx"})
+            {
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    object readOnly = false;
+                    object visible = true;
+                    object save = false;
+                    object fileName = ofd.FileName;
+                    object newTeamplate = false;
+                    object docType = 0;
+                    object missing = Type.Missing;
+
+                    Microsoft.Office.Interop.Word._Document document;
+                    Microsoft.Office.Interop.Word._Application application = new Microsoft.Office.Interop.Word.Application() { Visible = true};
+                    document = application.Documents.Open(ref fileName, ref missing, ref readOnly, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing,
+                        ref visible, ref missing, ref missing, ref missing, ref missing);
+                    document.ActiveWindow.Selection.WholeStory();
+                    document.ActiveWindow.Selection.Copy();
+                    IDataObject dataObject = Clipboard.GetDataObject();
+                    textToImport.Rtf = dataObject.GetData(DataFormats.Rtf).ToString();
+                    application.Quit(ref missing, ref missing, ref missing);
+                }
+            }
+        }
+       
+        private void export_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "" || textBox1.Text == " ")
+            {
+                MessageBox.Show("Numele fileului nu poate fi pustiu");
+            }
+            else
+            {
+                Microsoft.Office.Interop.Word.Application word = new Microsoft.Office.Interop.Word.Application();
+                word.Visible = true;
+                Microsoft.Office.Interop.Word.Document doc = word.Documents.Add();
+                Microsoft.Office.Interop.Word.Paragraph paragraph;
+                paragraph = doc.Paragraphs.Add();
+                paragraph.Range.Text = textToExport.Text;
+                Range docRange = doc.Range();
+
+                //using (FontDialog dialog = new FontDialog())
+                //{
+                //    if (dialog.ShowDialog() == DialogResult.OK)
+                //    {
+                //        paragraph.Range.Font = dialog.Font;
+                //    }
+                //}
+
+                if (checkBox1.Checked)
+                {
+                    string imagePath = @"D:\projects\wordAPI\cSharpIcon.png";
+
+                    object oMissing = System.Reflection.Missing.Value;
+                    object oEndOfDoc = "\\endofdoc";
+
+                    Microsoft.Office.Interop.Word.Range range = doc.Bookmarks.get_Item(ref oEndOfDoc).Range;
+                    range.Application.ActiveDocument.InlineShapes.AddPicture(imagePath);
+                }
+
+                doc.SaveAs2(@"D:\" + textBox1.Text + ".doc");
+                doc.Close();
+                word.Quit();
+                textToExport.Text = "";
+                textBox1.Text = "";
+
+
+            }
+        }
+    }
+}
